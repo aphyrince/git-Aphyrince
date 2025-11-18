b30676e7ca49af5786f71cf3a33f7b85692be6b6
aphyrince <dkswjdgh01@naver.com>
2025-11-18 20:05:37 +0900

    필요한 요소들 구상

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="app-container">
+            <div className="option">option</div>
+            <div className="section cmd-list">CMDLIST</div>
+            <div className="section history">history</div>
+            <div className="section dialog">dialog</div>
+        </div>
+    );
+};
 
 export default App;
