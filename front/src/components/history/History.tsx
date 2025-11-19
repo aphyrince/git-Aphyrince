58f16162ceb8113730dce5fceff85555c462ad88
aphyrince <dkswjdgh01@naver.com>
2025-11-19 19:58:54 +0900

    history 섹션 디자인

--- front/src/components/history/History.tsx
+++ front/src/components/history/History.tsx
@@ -1,7 +1,35 @@
-import React from "react";
 import "./History.css";
 const History = () => {
-    return <div className="section history">history</div>;
+    return (
+        <div className="section history">
+            <header>header</header>
+            <div className="history-content">
+                <div className="history-grid category">
+                    <p>graph</p>
+                    <p>comment</p>
+                    <p>date</p>
+                    <p>author</p>
+                    <p>commit</p>
+                </div>
+                <ul className="history-list">
+                    <li className="history-grid history-item">
+                        <p>graph</p>
+                        <p>comment</p>
+                        <p>date</p>
+                        <p>author</p>
+                        <p>commit</p>
+                    </li>
+                    <li className="history-grid history-item">
+                        <p>graph</p>
+                        <p>comment</p>
+                        <p>date</p>
+                        <p>author</p>
+                        <p>commit</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default History;
