bad457c713720eb80ae2b8e8b84d9130a8968b88
aphyrince <dkswjdgh01@naver.com>
2025-11-19 19:32:00 +0900

    option 창 디자인

--- front/src/components/option/Option.tsx
+++ front/src/components/option/Option.tsx
@@ -1,8 +1,21 @@
-import React from "react";
 import "./Option.css";
+import { BiSolidBrightness, BiSolidFilePlus } from "react-icons/bi";
 
 const Option = () => {
-    return <div className="option">option</div>;
+    return (
+        <div className="option">
+            <div className="option-section left">
+                <button className="repo-add-btn" title="add repository">
+                    <BiSolidFilePlus size={40} />
+                </button>
+            </div>
+            <div className="option-section right">
+                <button className="setting-btn" title="setting">
+                    <BiSolidBrightness size={40} />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Option;
