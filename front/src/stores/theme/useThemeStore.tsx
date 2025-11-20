09a977f9250fe11d570cddaa448cbd83f143d916
aphyrince <dkswjdgh01@naver.com>
2025-11-20 16:58:22 +0900

    theme 상태관리 스토어 만듬.

--- front/src/stores/theme/useThemeStore.tsx
+++ front/src/stores/theme/useThemeStore.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { create } from "zustand";
 
-const useThemeStore = () => {
-    return <div></div>;
-};
+interface Theme {
+    fontColor: string;
+    bgColor: string;
+    keyColor: string;
+}
+
+interface ThemeStatus {
+    currentTheme: Theme;
+    setTheme: (theme: Theme) => void;
+}
+
+const useThemeStore = create<ThemeStatus>((set) => ({
+    currentTheme: { fontColor: "#000", bgColor: "#fff", keyColor: "#aaa" },
+    setTheme: (theme: Theme) => {
+        set(() => ({ currentTheme: theme }));
+    },
+}));
 
 export default useThemeStore;
