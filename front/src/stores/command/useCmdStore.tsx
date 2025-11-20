928d5ceebe67dd16daba365d25c50dea2b7f4514
aphyrince <dkswjdgh01@naver.com>
2025-11-20 15:25:50 +0900

    cmdList 스토어 만듬

--- front/src/stores/command/useCmdStore.tsx
+++ front/src/stores/command/useCmdStore.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { create } from "zustand";
 
-const useCmdStore = () => {
-    return <div></div>;
-};
+interface Cmd {
+    text: string;
+    key: string;
+}
+
+interface CmdState {
+    cmdList: Cmd[];
+    addCmd: (cmd: string) => void;
+    deleteCmd: (key: string) => void;
+}
+
+const useCmdStore = create<CmdState>((set) => ({
+    cmdList: [],
+
+    addCmd: (cmd) => {
+        const newCmd: Cmd = { text: cmd, key: Date() };
+        set((state) => ({ cmdList: [...state.cmdList, newCmd] }));
+    },
+
+    deleteCmd: (key) => {
+        set((state) => ({
+            cmdList: state.cmdList.filter((c) => c.key !== key),
+        }));
+    },
+}));
 
 export default useCmdStore;
