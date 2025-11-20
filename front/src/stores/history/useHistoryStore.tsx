66f23b6647f37940d2b9e955d70b1b4c59df11a2
aphyrince <dkswjdgh01@naver.com>
2025-11-20 16:51:49 +0900

    history 상태관리 store 만듬

--- front/src/stores/history/useHistoryStore.tsx
+++ front/src/stores/history/useHistoryStore.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { create } from "zustand";
 
-const useHistoryStore = () => {
-    return <div></div>;
-};
+interface HistoryItem {
+    graph: string;
+    comment: string;
+    date: string;
+    author: string;
+    commit: string;
+    key: string;
+}
+
+interface HistoryState {
+    list: HistoryItem[];
+    addHistory: (history: HistoryItem) => void;
+    updateHistory: (history: HistoryItem) => void;
+}
+
+const useHistoryStore = create<HistoryState>((set) => ({
+    list: [],
+    addHistory: (history: HistoryItem) => {
+        history.key = Date();
+        set((state) => ({ list: [...state.list, history] }));
+    },
+    updateHistory: (history: HistoryItem) => {
+        set((state) => ({
+            list: state.list.map((item) =>
+                item.key === history.key ? history : item
+            ),
+        }));
+    },
+}));
 
 export default useHistoryStore;
